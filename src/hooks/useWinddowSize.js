254a09773b9722fa88a9d429b097b91fec7eba05
ADamian17 <adonisdesuvikdo@gmail.com>
2021-03-24 17:20:00 -0700

    added use window size

--- src/hooks/useWinddowSize.js
+++ src/hooks/useWinddowSize.js
@@ -7,6 +7,7 @@ import { useState, useEffect } from 'react';
 export const useWindowSize = () => {
   // Initialize state with undefined width/height so server and client renders match
   // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+  
   const [windowSize, setWindowSize] = useState({
     width: undefined,
     height: undefined,
